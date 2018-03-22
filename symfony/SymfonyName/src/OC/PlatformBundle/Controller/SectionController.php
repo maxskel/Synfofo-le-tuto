29e1ea4a3405782e4019be339560d3e7345db0a0
Maxime <max_skel@hotmail.com>
2018-03-21 23:23:56 -0400

    Mini Jeux dans app_dev.php/game
    Jeux des nombres. Plus haut ou plus bas?
    mini Exercices pour symfony

--- symfony/SymfonyName/src/OC/PlatformBundle/Controller/SectionController.php
+++ symfony/SymfonyName/src/OC/PlatformBundle/Controller/SectionController.php
@@ -3,19 +3,22 @@
 namespace OC\PlatformBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class SectionController extends Controller{
 	
-	public function homeAction(){
+	public function gameAction(Request $request){
 		
-		$content = $this->get('templating')->render("@OCPlatform/Section/article.html.twig", [
-   'articles' => [
-      ['title' => 'une maison rouge', 'content' => '<b>Beber</b> un éleveur de mouton à la campagne'],
-      ['title' => 'mr n\'importe nawak', 'content' => "Plusieurs variations de Lorem Ipsum peuvent être trouvées ici ou là, mais la majeure partie d'entre elles a été altérée par l'addition d'humour ou de mots aléatoires qui ne ressemblent pas une seconde à du texte standard. Si vous voulez utiliser un passage du Lorem Ipsum, vous devez être sûr qu'il n'y a rien d'embarrassant caché dans le texte. Tous les générateurs de Lorem Ipsum sur Internet tendent à reproduire le même extrait sans fin, ce qui fait de lipsum.com le seul vrai générateur de Lorem Ipsum. Iil utilise un dictionnaire de plus de 200 mots latins, en combinaison de plusieurs structures de phrases, pour générer un Lorem Ipsum irréprochable. Le Lorem Ipsum ainsi obtenu ne contient aucune répétition, ni ne contient des mots farfelus, ou des touches d'humour."],
-   ],] );
+		$text = $request->request->get("text");
+		$random = $request->request->get("randomNumber");
 		
-		return new Response($content);
+		if($text == null || $text == -1){
+			$random = rand(0,99);
+			$text = 0;
+		}
+		
+		return $this->render("@OCPlatform/Section/article.html.twig", ["text" => $text , "randomNumber" => $random ]);
 	}
 	
 }
